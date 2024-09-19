7181f6982d4c074a2dd5e068229cb698923755f7
AndreyBoykov <yaroslavl92@mail.com>
2024-09-19 19:03:38 +0300

    first commit

---
+++ client/vite.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+      '/img': 'http://localhost:3000',
+    },
+  },
+  build: {
+    outDir: '../server/dist',
+  },
+  base: '/',
+});
